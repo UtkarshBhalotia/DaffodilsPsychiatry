b94620f731141d38af5fd033d2839fc51e1d4bfc
BusyInfotech <biplapps@gmail.com>
2021-12-30 00:09:03 +0530

    1st Commit 30th Dec, 21

--- app/src/main/java/com/daffodils/psychiatry/activity/RegisterActivity.java
+++ app/src/main/java/com/daffodils/psychiatry/activity/RegisterActivity.java
@@ -1,6 +1,11 @@
 package com.daffodils.psychiatry.activity;
 
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class RegisterActivity extends AppCompatActivity {
+
+    TextView txtModule;
+
 }
